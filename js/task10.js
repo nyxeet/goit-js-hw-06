e96b902096b0a873cad8b324acb9e8161f1f9340
nyxeet <nyxeet@ukr.net>
2020-11-18 20:10:02 +0200

    sa

---
+++ js/task10.js
@@ -0,0 +1,17 @@
+const getSortedUniqueSkills = users => {
+    const getSkills = [];
+    users.map(function (user) {
+        user.skills.map(function (skill) {
+            if (!getSkills.some(newSkill => newSkill === skill)) {
+                getSkills.push(skill);
+            }
+            
+            
+        })
+    })
+    return getSkills.sort();
+
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
