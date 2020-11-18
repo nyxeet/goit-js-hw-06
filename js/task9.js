c1f63a4925240e380aa4932372934602408354fa
nyxeet <nyxeet@ukr.net>
2020-11-18 19:50:02 +0200

    9

---
+++ js/task9.js
@@ -0,0 +1,15 @@
+const getNamesSortedByFriendsCount = users => {
+    const newUsers = [...users]; // чтобы не изменялся дефолтный массив
+    const namesUsers = [];
+    newUsers.sort(function (prevUser, nextUser) {
+        return prevUser.friends.length - nextUser.friends.length;
+        
+    })
+    newUsers.map(function (user) {
+        namesUsers.push(user.name);
+    })
+    return namesUsers;
+};
+
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
