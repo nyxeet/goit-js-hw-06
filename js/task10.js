e469b1eebd4f4dded6c754bca7483915ca56b587
nyxeet <nyxeet@ukr.net>
2020-11-19 13:44:46 +0200

    Update task10.js

--- js/task10.js
+++ js/task10.js
@@ -1,14 +1,22 @@
 const getSortedUniqueSkills = users => {
-    const getSkills = [];
-    users.map(function (user) {
-        user.skills.forEach(function (skill) {
-            if (!getSkills.some(newSkill => newSkill === skill)) {
-                getSkills.push(skill);
-            }   
-        })
-    })
-    return getSkills.sort();
+    // const getSkills = [];
+    // users.map(function (user) {
+    //     user.skills.forEach(function (skill) {
+    //         if (!getSkills.some(newSkill => newSkill === skill)) {
+    //             getSkills.push(skill);
+    //         }   
+    //     })
+    // })
+    // return getSkills.sort();
 
+    return users.reduce(function (allSkills, user) {
+        user.skills.forEach(function (skill) {
+            if (!allSkills.includes(skill)) {
+                allSkills.push(skill);
+            }
+        })
+        return allSkills;
+    }, []);
 };
 
 console.log(getSortedUniqueSkills(users));
